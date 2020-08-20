522495bb9822be801639f1e3e8c7bff153265826
pislaBos <57287326+pislaBos@users.noreply.github.com>
2020-08-20 11:09:47 +0300

    Working with API's

---
+++ API/first_request.js
@@ -0,0 +1,12 @@
+const request = require("request");
+
+request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) =>
+{   
+   
+    if(!error && response.statusCode == 200)
+    {
+        // THINGS WORKED!
+        const parsedData = JSON.parse(body);
+        console.log(parsedData.name + " lives in " + parsedData.address.city);
+    }
+});
